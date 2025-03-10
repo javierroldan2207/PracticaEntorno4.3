87c1934f61f3b856b91ddd176c94c2f6489a79c5
Javierroldan2207 <javierroldan2207@gmail.com>
2025-03-10 12:19:28 +0100

    Cambios en test Usuario

--- UsuarioCredencial/JavierRoldan1/Test/test/TestCredencial.java
+++ UsuarioCredencial/JavierRoldan1/Test/test/TestCredencial.java
@@ -1,6 +1,8 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,25 +10,34 @@ import com.auth.Credencial;
 
 class TestCredencial {
 
-	Credencial credencial = new Credencial("user123", "pass123", "0");
-
-    @Test
-    void testConstructor() {
-        assertEquals("user123", credencial.getUsurname(), "El username no se asignó correctamente.");
+	Credencial credencial = new Credencial("Juan", "Pérez", "Contraseña123");
+	
+	
+	@Test
+    public void testGenerarUsername() {
+        String expectedUsername = "juape100";
+        assertEquals(expectedUsername, credencial.getUsurname(), "El username generado es incorrecto");
     }
 
     @Test
-    void testSetPassword() {
-        credencial.setPassword("newPass456");
+    public void testEsPasswordSegura() {
+        assertTrue(credencial.esPasswordSegura(), "La contraseña debería ser segura");
     }
 
     @Test
-    void testGenerarUsername() {
-        String username1 = credencial.generarUsername("Carlos", "Gomez");
-        String username2 = credencial.generarUsername("Luis", "Fernandez");
+    public void testEsPasswordNoSegura() {
+        Credencial credencialNoSegura = new Credencial("Ana", "Gómez", "contraseña");
+        assertFalse(credencialNoSegura.esPasswordSegura(), "La contraseña debería no ser segura");
+    }
 
-        assertEquals("cargom0", username1, "Error en el username generado.");
-        assertEquals("luifer1", username2, "Error en el username generado.");
+    @Test
+    public void testComprobarPassword() {
+        assertTrue(credencial.comprobarPassword("Contraseña123"), "Las contraseñas deberían coincidir");
+    }
+
+    @Test
+    public void testComprobarPasswordIncorrecta() {
+        assertFalse(credencial.comprobarPassword("ContraseñaIncorrecta"), "Las contraseñas no deberían coincidir");
     }
 
 }
